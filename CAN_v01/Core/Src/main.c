6719250cc0a46da1098aabe7c564d1fb6b63791f
szawadzka <zawadzka.slw@gmail.com>
2021-11-17 13:05:37 +0100

    change CAN_tx and CAN rx pins, also init configuration created in ioe

--- CAN_v01/Core/Src/main.c
+++ CAN_v01/Core/Src/main.c
@@ -103,7 +103,7 @@ int main(void)
   while (1)
   {
 
-	 if(button=1){
+	 if(button==1){
 		 HAL_Delay(500);
 		 HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
 		 button=0;
@@ -178,15 +178,15 @@ static void MX_CAN1_Init(void)
 
   /* USER CODE END CAN1_Init 1 */
   hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 16;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.Prescaler = 100;
+  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
+  hcan1.Init.AutoRetransmission = ENABLE;
   hcan1.Init.ReceiveFifoLocked = DISABLE;
   hcan1.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
@@ -194,11 +194,7 @@ static void MX_CAN1_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN CAN1_Init 2 */
-  hcan1.Init.Prescaler = 100;
-  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
-  hcan1.Init.AutoRetransmission = ENABLE;
-  hcan1.Init.TimeSeg1 = CAN_BS1_13TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
+
 
   /* USER CODE END CAN1_Init 2 */
 
